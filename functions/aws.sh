0224f46207e1549dfff501269db1a2d3c01f57ad
João Paulo Seregatte Costa <joao.costa@itelios.com>
2020-04-23 18:48:43 -0300

    AWS function

--- functions/aws.sh
+++ functions/aws.sh
@@ -1,4 +1,4 @@
 function +awsvars(){
 	cat ~/.aws/credentials | sed -E 's%^[^=]*=%%' | sed -E 's%\[(.*)\]%\1%' | xargs -n3 | grep $1 | awk '{ 
-print "export AWS_DEFAULT_REGION\=\"us-east-1\"\n\export\ AWS_ACCESS_KEY_ID=\""$2"\"\n\export AWS_SECRET_ACCESS_KEY=\""$3"\"\n\# Run this command to configure "$1" in your shell:\n\# eval $(+awsvars "$1")\n\n" }'	
+print "export AWS_PROFILE\=\""$1"\"\nexport AWS_DEFAULT_REGION\=\"us-east-1\"\n\export\ AWS_ACCESS_KEY_ID=\""$2"\"\n\export AWS_SECRET_ACCESS_KEY=\""$3"\"\n\# Run this command to configure "$1" in your shell:\n\# eval $(+awsvars "$1")\n\n" }'	
 }
